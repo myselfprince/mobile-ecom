b7be3dff92f74e189b472a50bba4c9636234376a
myselfprince <contactprincemedia@gmail.com>
2023-11-20 21:03:23 +0530

    added mongoDB

--- components/Blocks/MobileBlock.js
+++ components/Blocks/MobileBlock.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-const MobileBlock = () => {
+const MobileBlock = (props) => {
   return (
     <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
       <Link href="/products/mobile-phone">
@@ -10,7 +10,7 @@ const MobileBlock = () => {
     </div>
     <div className="mt-4 text-center">
       <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Mobile</h3>
-      <h2 className="text-gray-900 title-font text-lg font-medium">Oneplus 11R</h2>
+      <h2 className="text-gray-900 title-font text-lg font-medium">{props.item}</h2>
       <p className="mt-1">₹699</p>
     </div></Link>
   </div>
--- pages/mobile.js
+++ pages/mobile.js
@@ -1,31 +1,52 @@
-import React from 'react'
-import MobileBlock from '../components/Blocks/MobileBlock'
+import React from "react";
+import MobileBlock from "../components/Blocks/MobileBlock";
+import Link from "next/link";
+import Product from "../models/Product";
+import connectDb from "../middleware/mongoose";
+import mongoose from "mongoose";
 
-const Mobile = () => {
+const Mobile = ({products}) => {
   return (
     <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4">
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-      <MobileBlock />
-    </div>
-  </div>
-</section>
-  )
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+        {products.map((item)=>{ return <div key={item._id} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <Link passHref={true} href={`/products/${item.slug}`}>
+              <div className="block flex justify-center items-center relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-top"
+                  src={item.img}
+                />
+              </div>
+              <div className="mt-4 text-center">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  Mobile
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  {item.title}
+                </h2>
+                <p className="mt-1">₹{item.price}</p>
+              </div>
+            </Link>
+          </div>})}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+
+
+  let products = await Product.find({category: 'mobile'});
+
+  return {
+    props: {products: JSON.parse(JSON.stringify(products))},
+  };
 }
 
-export default Mobile
+export default Mobile;
