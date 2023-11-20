d9abb08abe9bd11bd0f2ee762e934bfee30cfe38
myselfprince <contactprincemedia@gmail.com>
2023-11-20 15:18:28 +0530

    fixed error

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useRouter } from "next/router"
+import { useState } from "react"
  
 export default function Page({addToCart}) {
   const router = useRouter();
@@ -9,7 +9,7 @@ export default function Page({addToCart}) {
 
 
   const checkServiceability= async()=>{
-    let pins = await fetch('http://localhost:3000/api/pincode')
+    let pins = await fetch("http://localhost:3000/api/pincode")
     let pinJson = await pins.json()
     if(pinJson.includes(parseInt(pin))){
       setService(true)
@@ -97,7 +97,7 @@ export default function Page({addToCart}) {
         <div className="flex">
           <span className="title-font font-medium text-2xl text-gray-900 my-auto">₹499</span>
           <button className="flex ml-2 sm:ml-10 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy Now</button>
-          <button onClick={()=>{addToCart(slug, 1, 499, 'Apple iphone(XL, Red)','XL','Red')}} className="flex ml-2 sm:ml-10 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
+          <button onClick={()=>{addToCart(slug, 1, 499, "Apple iphone(XL, Red)","XL","Red")}} className="flex ml-2 sm:ml-10 text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
           {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
@@ -120,7 +120,7 @@ export default function Page({addToCart}) {
 
             </div>
             <div className='mt-3'>
-              {(!service && service!=null) && <p className='text-red-600'>We can't deliver to this pincode</p>}
+              {(!service && service!=null) && <p className='text-red-600'>We cant deliver to this pincode</p>}
               {(service && service!=null) &&<p className='text-green-600'>Pincode Delivery Available</p>}
             </div>
 
