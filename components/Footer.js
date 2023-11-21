90b09cf5a4219af91a8f53d298974f5859dc4210
myselfprince <contactprincemedia@gmail.com>
2023-11-21 10:21:42 +0530

    working website endpoint

--- components/Footer.js
+++ components/Footer.js
@@ -14,79 +14,67 @@ const Footer = () => {
     </div>
     <div className="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">EXPLORE</h2>
         <nav className="list-none mb-10">
           <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Mobiles</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Tablets</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Brands</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Accessories</a>
           </li>
+         
         </nav>
       </div>
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ACCOUNT</h2>
         <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
+        <li>
+            <a className="text-gray-600 hover:text-gray-800">Login</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Signup</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Contact</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+            <a className="text-gray-600 hover:text-gray-800">About</a>
           </li>
+        
         </nav>
       </div>
+
       <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">POLICY</h2>
         <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
+        <li>
+            <a className="text-gray-600 hover:text-gray-800">Terms & Condition</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Return Policy</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Delivery Policy</a>
           </li>
           <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+            <a className="text-gray-600 hover:text-gray-800">Refund Policy</a>
           </li>
+        
         </nav>
       </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
+    
     </div>
   </div>
   <div className="bg-gray-100">
     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-      <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+      <p className="text-gray-500 text-sm text-center sm:text-left">© 2023 MobileMart.com
+       
       </p>
       <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
         <a className="text-gray-500">
--- pages/mobile.js
+++ pages/mobile.js
@@ -10,13 +10,13 @@ const Mobile = ({products}) => {
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
-        {products.map((item)=>{ return <div key={item._id} className="lg:w-1/4 md:w-1/2 p-4 w-full">
-            <Link passHref={true} href={`/products/${item.slug}`}>
+        {Object.keys(products).map((item)=>{ return <div key={products[item]._id} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <Link passHref={true} href={`/products/${products[item].slug}`}>
               <div className="block flex justify-center items-center relative rounded overflow-hidden">
                 <img
                   alt="ecommerce"
                   className="object-cover object-top"
-                  src={item.img}
+                  src={products[item].img}
                 />
               </div>
               <div className="mt-4 text-center">
@@ -24,9 +24,9 @@ const Mobile = ({products}) => {
                   Mobile
                 </h3>
                 <h2 className="text-gray-900 title-font text-lg font-medium">
-                  {item.title}
+                  {products[item].title}
                 </h2>
-                <p className="mt-1">₹{item.price}</p>
+                <p className="mt-1">₹{products[item].price}</p>
               </div>
             </Link>
           </div>})}
@@ -43,9 +43,30 @@ export async function getServerSideProps(context) {
 
 
   let products = await Product.find({category: 'mobile'});
+  let mobiles={}
+
+    for(let item of products){
+        if(item.title in mobiles){
+            if(!mobiles[item.title].color.includes(item.color) && item.availableQty>0){
+                mobiles[item.title].color.push(item.color)
+            }
+            if(!mobiles[item.title].size.includes(item.size) && item.availableQty>0){
+                mobiles[item.title].size.push(item.size)
+            }
+
+        }
+        else{
+            mobiles[item.title] = JSON.parse(JSON.stringify(item))
+            if(item.availableQty>0){
+                mobiles[item.title].color = [item.color]
+                mobiles[item.title].size = [item.size]
+            }
+        }
+    }
+
 
   return {
-    props: {products: JSON.parse(JSON.stringify(products))},
+    props: {products: JSON.parse(JSON.stringify(mobiles))},
   };
 }
 
